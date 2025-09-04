395bdfb7a502cb1c598558342ef39105d674367e
Atharva <atharvashah7@gmail.com>
2025-09-04 19:07:36 +0530

    high score is now saved with the help files

--- score.py
+++ score.py
@@ -3,7 +3,7 @@ class ScoreCard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
-        self.high_score=0
+        self.high_score=self.get_highscore()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -18,8 +18,15 @@ class ScoreCard(Turtle):
     def reset(self):
         if self.score>self.high_score:
             self.high_score=self.score
+            self.update_highscore(self.high_score)
         self.score=0
         self.update_score()
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER",align="center",font=("Arial",24,"normal"))
+    def get_highscore(self):
+        with open("data.txt","r")as file:
+            return int(file.read())
+    def update_highscore(self,new_highscore):
+        with open("data.txt","w")as file:
+            file.write(str(new_highscore))
